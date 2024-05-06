02b95c875c237ef419701f35a0ea948f12e72ba2
CaioAdania <caioadania@gmail.com>
2024-05-06 19:17:15 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Estacionamento.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entities
+{
+    internal class Estacionamento
+    {
+        private List<Carro> carrosEstacionados;
+
+        public Estacionamento()
+        {
+            carrosEstacionados = new List<Carro>();
+        }
+
+        public void AdicionarCarro(Carro carro)
+        {
+            carrosEstacionados.Add(carro);
+            Console.WriteLine("Carro adicionado ao estacionamento.");
+        }
+
+        public void RemoverCarro(string placa)
+        {
+            Carro carroRemovido = carrosEstacionados.Find(carro => carro.Placa == placa);
+            if (carroRemovido != null)
+            {
+                carrosEstacionados.Remove(carroRemovido);
+                Console.WriteLine("Carro removido do estacionamento.");
+            }
+            else
+            {
+                Console.WriteLine("Carro não encontrado no estacionamento.");
+            }
+        }
+
+        public void ExibirCarrosEstacionados()
+        {
+            if (carrosEstacionados.Count > 0)
+            {
+                Console.WriteLine("Carros estacionados:");
+                foreach (Carro carro in carrosEstacionados)
+                {
+                    Console.WriteLine($"Marca: {carro.Marca}, Modelo: {carro.Modelo}, Placa: {carro.Placa}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há carros estacionados no momento.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using ConsoleApp1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estacionamento estacionamento = new Estacionamento();
+
+            while (true)
+            {
+                Console.WriteLine("\nEscolha uma opção:");
+                Console.WriteLine("1. Adicionar carro");
+                Console.WriteLine("2. Remover carro");
+                Console.WriteLine("3. Exibir carros estacionados");
+                Console.WriteLine("4. Sair");
+
+                int opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Digite a marca do carro: ");
+                        string marca = Console.ReadLine();
+                        Console.Write("Digite o modelo do carro: ");
+                        string modelo = Console.ReadLine();
+                        Console.Write("Digite a placa do carro: ");
+                        string placa = Console.ReadLine();
+
+                        Carro novoCarro = new Carro() { Marca = marca, Modelo = modelo, Placa = placa };
+                        estacionamento.AdicionarCarro(novoCarro);
+                        break;
+                    case 2:
+                        Console.Write("Digite a placa do carro a ser removido: ");
+                        string placaRemover = Console.ReadLine();
+                        estacionamento.RemoverCarro(placaRemover);
+                        break;
+                    case 3:
+                        estacionamento.ExibirCarrosEstacionados();
+                        break;
+                    case 4:
+                        Console.WriteLine("Fechando programa");
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+        }
+    }
+}
